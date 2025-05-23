2585ba525e8232d78c62cbd33382a4f9bdb76243
Julia-48 <juliasoares230897@gmail.com>
2025-05-23 18:54:50 +0000

    Aula 23/05/2025 - Props, Children e renderização usando map

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import "./styles/theme.css";
 import "./styles/global.css";
+import { MyText } from "./components/MyText";
 
-function App() {
+export default function App() {
+
+const texts = [
+  {
+    title: "Meu título",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam recusandae voluptatem, soluta tempora, vitae modi nihil, quos maxime maiores fugit sit reiciendis odio commodi? Doloremque excepturi dignissimos exercitationem laborum suscipit.",
+  },
+  {
+    title: "Meu título",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam recusandae voluptatem, soluta tempora, vitae modi nihil, quos maxime maiores fugit sit reiciendis odio commodi? Doloremque excepturi dignissimos exercitationem laborum suscipit.",
+  },
+  {
+    title: "Meu título",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam recusandae voluptatem, soluta tempora, vitae modi nihil, quos maxime maiores fugit sit reiciendis odio commodi? Doloremque excepturi dignissimos exercitationem laborum suscipit.",
+  },
+];
+
   return (
-    <div>
-      <h1>Meu pirmeiro React app</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam
-        recusandae voluptatem, soluta tempora, vitae modi nihil, quos maxime
-        maiores fugit sit reiciendis odio commodi? Doloremque excepturi
-        dignissimos exercitationem laborum suscipit.
-      </p>
-    </div>
+    //React Fragment
+    <>
+    {texts.map((text, index) => (
+      <MyText key={index} title={`${index + 1} - ${text.title}`}>
+        {text.text}
+      </MyText>
+    ))}
+    </>
   );
 }
-
-export default App;
