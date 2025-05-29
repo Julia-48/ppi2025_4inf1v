1ace973edfedc8411458661b77843949f4bfdcf3
Julia-48 <juliasoares230897@gmail.com>
2025-05-29 19:08:32 +0000

    Aula 29/05/2025 - css flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,11 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { MyText } from "./components/MyText";
+import { MyTextList } from "./components/MyTextList";
 
 export default function App() {
-
-const texts = [
-  {
-    title: "Meu título",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam recusandae voluptatem, soluta tempora, vitae modi nihil, quos maxime maiores fugit sit reiciendis odio commodi? Doloremque excepturi dignissimos exercitationem laborum suscipit.",
-  },
-  {
-    title: "Meu título",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam recusandae voluptatem, soluta tempora, vitae modi nihil, quos maxime maiores fugit sit reiciendis odio commodi? Doloremque excepturi dignissimos exercitationem laborum suscipit.",
-  },
-  {
-    title: "Meu título",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam recusandae voluptatem, soluta tempora, vitae modi nihil, quos maxime maiores fugit sit reiciendis odio commodi? Doloremque excepturi dignissimos exercitationem laborum suscipit.",
-  },
-];
-
   return (
-    //React Fragment
     <>
-    {texts.map((text, index) => (
-      <MyText key={index} title={`${index + 1} - ${text.title}`}>
-        {text.text}
-      </MyText>
-    ))}
+      <MyTextList />
     </>
   );
 }
---
+++ src/components/MyTextList.jsx
@@ -0,0 +1,55 @@
+import styles from "./MyTextList.module.css";
+import { MyText } from "./MyText";
+
+export function MyTextList() {
+  const texts = [
+    {
+      title: "Meu título",
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam recusandae voluptatem, soluta tempora, vitae modi nihil, quos maxime maiores fugit sit reiciendis odio commodi? Doloremque excepturi dignissimos exercitationem laborum suscipit.",
+    },
+    {
+      title: "Meu título",
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam recusandae voluptatem, soluta tempora, vitae modi nihil, quos maxime maiores fugit sit reiciendis odio commodi? Doloremque excepturi dignissimos exercitationem laborum suscipit.",
+    },
+    {
+      title: "Meu título",
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam recusandae voluptatem, soluta tempora, vitae modi nihil, quos maxime maiores fugit sit reiciendis odio commodi? Doloremque excepturi dignissimos exercitationem laborum suscipit.",
+    },
+    {
+      title: "Meu título",
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam recusandae voluptatem, soluta tempora, vitae modi nihil, quos maxime maiores fugit sit reiciendis odio commodi? Doloremque excepturi dignissimos exercitationem laborum suscipit.",
+    },
+    {
+      title: "Meu título",
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam recusandae voluptatem, soluta tempora, vitae modi nihil, quos maxime maiores fugit sit reiciendis odio commodi? Doloremque excepturi dignissimos exercitationem laborum suscipit.",
+    },
+    // {
+    //   title: "Meu título",
+    //   text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam recusandae voluptatem, soluta tempora, vitae modi nihil, quos maxime maiores fugit sit reiciendis odio commodi? Doloremque excepturi dignissimos exercitationem laborum suscipit.",
+    // },
+    // {
+    //   title: "Meu título",
+    //   text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam recusandae voluptatem, soluta tempora, vitae modi nihil, quos maxime maiores fugit sit reiciendis odio commodi? Doloremque excepturi dignissimos exercitationem laborum suscipit.",
+    // },
+    // {
+    //   title: "Meu título",
+    //   text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam recusandae voluptatem, soluta tempora, vitae modi nihil, quos maxime maiores fugit sit reiciendis odio commodi? Doloremque excepturi dignissimos exercitationem laborum suscipit.",
+    // },
+    // {
+    //   title: "Meu título",
+    //   text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam recusandae voluptatem, soluta tempora, vitae modi nihil, quos maxime maiores fugit sit reiciendis odio commodi? Doloremque excepturi dignissimos exercitationem laborum suscipit.",
+    // },
+  ];
+
+  return (
+    //React Fragment
+
+    <div className={styles.container}>
+      {texts.map((text, index) => (
+        <MyText key={index} title={`${index + 1} - ${text.title}`}>
+          {text.text}
+        </MyText>
+      ))}
+    </div>
+  );
+}
