3a3b855eb5d06725030f331e4681ee88c7ce3e8f
Julia-48 <juliasoares230897@gmail.com>
2025-07-04 19:00:15 +0000

    Aula 04/07 - Desafio 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { MyTextList } from "./components/MyTextList";
-import { Main } from "./components/Main";
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
-import { LuckyNumber } from "./components/LuckyNumber";
+import { ProductList } from "./components/ProductList.jsx";
+
 
 export default function App() {
   return (
     <>
-      <LuckyNumber />
-    </>
+      <ProductList />
+    </> 
   );
 }
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+import styles from './ProductList.module.css';
+import { CircularProgress } from '@mui/material';
+
+export function ProductList() {
+    var category = "smartphones";
+    var limit = 10;
+    var apiUrl = `https://dummyjson.com/products/category/${category}?limit=${limit}&select=id,thumbnail,brand,title,price,description`;
+
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function fetchProducts(){
+            try{
+                const response = await fetch(apiUrl);
+                const data = await response.json();
+                setProducts(data.products);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchProducts();
+    }, []);
+
+    return (
+    <div className={StyleSheet.container}>
+        <h1>TJA Megastore</h1>
+        {products.map((product) => (
+            <div key={product.id} className={styles.product}>
+                <img 
+                    src={product.thumbnail}
+                    alt={product.title}
+                    className={styles.productImage} 
+                />
+                <h2 className={styles.productTitle}>{`${product.brand} ${product.title}`}</h2>
+                <p className={styles.productPrice}>Price: ${product.price}</p>
+                <p className={styles.productDescription}>{product.description}</p>
+            </div>
+        ))}
+        {loading && (
+            <div>
+                <CircularProgress
+                    thickness={5}
+                    style={{ margin: "2rem auto", display: "block" }}
+                    sx={{ color: "var(--primary-color)" }}
+                />
+                <p>Loading products...</p>
+            </div>
+        )}
+    </div>
+    );
+}
