d842dddf6d7a0fc709c26334c39c078dcd5a0ae3
Julia-48 <juliasoares230897@gmail.com>
2025-08-13 18:22:44 +0000

    Desafio Final - 2° Bim

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 import { Manager } from "./components/Manager";
+import { Cadastro } from "./components/Cadastro";
+import { Login } from "./components/Login";
 
 export default function App() {
 
   return (
     <>
+      <Login />
+      <Cadastro />
       <Manager />
     </>
   );
